2bbd580798c116cc50ffa9595568063250cb764d
Jim Schubert <james.schubert@gmail.com>
2012-06-15 18:20:20 -0400

    Added code pieces for blog at http://www.ipreferjim.com/2012/06/debug-writeline

---
+++ 2012-05-15/Program.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Diagnostics;
+
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Console.WriteLine #1");
+        Debug.WriteLine("Debug.WriteLine");
+        Console.WriteLine("Console.WriteLine #2");
+    }
+}
